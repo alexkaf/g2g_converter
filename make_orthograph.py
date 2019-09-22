3def44c02183842040e927d4562e90eb56c0d566
Alexandros Kafiris <alexkafiris@gmail.com>
2019-09-22 15:52:44 +0300

    converter

--- make_orthograph.py
+++
@@ -1,9 +0,0 @@
-import dict_funcs2 as orthograph
-import fst_make as fst
-
-fst.make_of_I2()
-rulesbook = orthograph.errors_find("train_co.txt","train_wr.txt",2)
-fst.Epsilon(rulesbook)
-
-(cost_del,cost_sub,cost_swap,cost_ins) = orthograph.errors_find("train_co.txt","train_wr.txt",1)
-fst.make_of_E(cost_swap,cost_del,cost_ins,cost_sub)
